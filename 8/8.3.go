f2920e37970afe289811009d51dc4ca8839de521
George Bramus <george.bramus@gmail.com>
2022-09-05 12:13:49 +0300

    8.3 Сдача

---
+++ 8/8.3.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+func lemonadeChange(bills []int) bool {
+	var five, ten int
+	for _, bill := range bills {
+		switch bill {
+		case 5:
+			five++
+		case 10:
+			ten++
+		}
+		if bill == 10 {
+			if five > 0 {
+				five--
+				continue
+			} else {
+				return false
+			}
+		} else if bill == 20 {
+			if ten > 0 && five > 0 {
+				five--
+				ten--
+				continue
+			} else if five >= 3 {
+				five -= 3
+				continue
+			} else {
+				return false
+			}
+		}
+
+	}
+	return true
+}
+
+func main() {
+	var (
+		bills []int
+		bill  int
+	)
+	numberClient := 1
+	fmt.Println("(!) При вводе нуля для очередного клиента, цикл завершится.")
+	for {
+		fmt.Printf("Какая купюра у клиента №%d? : ", numberClient)
+		_, err := fmt.Scan(&bill)
+		if bill > 0 && err != io.EOF {
+			bills = append(bills, bill)
+		} else {
+			break
+		}
+
+		numberClient++
+	}
+
+	fmt.Println(lemonadeChange(bills))
+
+}
